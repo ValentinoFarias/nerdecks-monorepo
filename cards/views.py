55c754ebb3db72e4b1e08abd8bf84029134c9006
ValentinoFarias <valentinofariascarrion@gmail.com>
2026-02-10 12:53:12 +0000

    adding signup functionality

--- cards/views.py
+++ cards/views.py
@@ -1,10 +1,40 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.views import View
+from django.contrib import messages
 
 
 class LandingPageView(TemplateView):
-	template_name = 'landingPage.html'
+    template_name = "landingPage.html"
 
 
 class HomeView(TemplateView):
-	template_name = 'home.html'
+    template_name = "home.html"
 
+
+class SignupView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "signup.html", {"form": form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+
+            messages.success(
+                request,
+                f"Account created successfully. Welcome, {user.username}!"
+            )
+
+            return redirect("home")
+
+        messages.error(
+            request,
+            "Something went wrong. Please check the form and try again."
+        )
+
+        return render(request, "signup.html", {"form": form})
--- nerdeck_project/urls.py
+++ nerdeck_project/urls.py
@@ -1,25 +1,10 @@
-"""
-URL configuration for nerdeck_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from cards.views import LandingPageView, HomeView
+from cards.views import LandingPageView, HomeView, SignupView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', HomeView.as_view(), name='home'),
-    path('', LandingPageView.as_view(), name='landingpage'),
+    path("admin/", admin.site.urls),
+    path("home/", HomeView.as_view(), name="home"),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("", LandingPageView.as_view(), name="landingpage"),
 ]
